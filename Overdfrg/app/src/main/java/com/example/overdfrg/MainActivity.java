fbea2e56f51b9446a4315a2fffa789b7fe1d227e
Fz624213336 <1624213336@qq.com>
2019-12-12 11:52:31 +0800

    Overdfrg

--- Overdfrg/app/src/main/java/com/example/overdfrg/MainActivity.java
+++ Overdfrg/app/src/main/java/com/example/overdfrg/MainActivity.java
@@ -3,11 +3,14 @@ package com.example.overdfrg;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.overdfrg.BaseActivity.BaseActivatey;
+
+public class MainActivity extends BaseActivatey {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
